5fc083f1c52ec9a22311e6641ad6e2ed277c4480
Yeon85 <icanspeak85@gmail.com>
2025-05-16 19:05:30 +0900

    수정

--- src/application.ts
+++ src/application.ts
@@ -2,7 +2,7 @@
 
 const ApplicationConfig = {
     // API 서버 주소
-    API_URL: import.meta.env.VITE_API_URL || 'https://bootcampbackend-production.up.railway.app',
+    API_URL: import.meta.env.VITE_API_BASE_URL || 'https://bootcampbackend-production.up.railway.app',
   
     // 백엔드 기본 URL (로컬 개발용)
     //API_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
